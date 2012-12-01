270495d5cfd183368de2e23a8c1f0da7e890c676
vince <vince@localhost>
2012-12-01 19:08:21 +0100

    expande/collapse

--- javascripts/omnix.js
+++ javascripts/omnix.js
@@ -10,9 +10,13 @@ var $container=$('#chromebay-ominx-container');
 if(!$container[0]){
 	$(document.body).append(
 		'<div id="chromebay-ominx-container">'+
-			'<audio id="chromebay-audio"></audio>'+
-			'<div class="head"><span>扇贝Sidekick</span><a href="javascript:void(0)" class="close" title="关闭">[x]</a></div>'+
+			'<div style="position:relative">'+
+			'<a href="javascript:void(0)" class="close" style="position:absolute;top:5px;right:10px;" title="关闭">x</a>'+
 			'<div class="content"></div>'+
+			'<div id="chromebay-ominx-separator">'+
+			'<img src="'+chrome.extension.getURL('images/collapse.png')+'" width="33" height="67"/>'+
+			'</div>'+
+			'</div>'+
 		'</div>'
 	);
 	$container=$('#chromebay-ominx-container');
@@ -20,7 +24,7 @@ if(!$container[0]){
 	if($container.css('left')=='-400px'){
 		$container.animate({
 			left:0
-		},1000);
+		},1000).find('#chromebay-ominx-separator img').attr('src',chrome.extension.getURL('images/collapse.png'));
 	}
 }
 
@@ -30,7 +34,7 @@ $(document.body).click(function(e){
 	if($(e.target).parents('#chromebay-ominx-container').get(0) == undefined && $container.css('left')=='0px'){
 		$container.animate({
 			left:-400
-		},1000);
+		},1000).find('#chromebay-ominx-separator img').attr('src',chrome.extension.getURL('images/expande.png'));
 	}
 
 });
@@ -39,7 +43,7 @@ $container.mouseover(function(){
 	if($container.css('left')=='-400px'){
 		$container.animate({
 			left:0
-		},1000);
+		},1000).find('#chromebay-ominx-separator img').attr('src',chrome.extension.getURL('images/collapse.png'));
 	}
 });	
 
